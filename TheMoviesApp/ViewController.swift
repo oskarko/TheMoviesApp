b7669e532fd10ec153c3b0abf85f84e60a23bbab
Óscar Rodríguez <oscar.garrucho@gmail.com>
2020-09-02 16:06:30 +0200

    Initial Commit

---
+++ TheMoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesApp
+//
+//  Created by Oscar Rodriguez Garrucho on 02/09/2020.
+//  Copyright © 2020 Little Monster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
